f2ad08a5f69d06a25230aa91acece5e4bdfd7619
Willy <willy@correomodificado.com>
2020-10-05 20:45:32 -0500

    listado y registro blog

---
+++ FrontEndBlog/src/js/index.js
@@ -0,0 +1,28 @@
+var app = angular.module('BlogApp',['ui.router']);
+app.controller('BlogAppCller',function($scope){
+    $scope.guardarEntrada=function(valores){
+        console.log(valores);
+    }
+})
+app.config(function($stateProvider){
+    var nuevaEntrada={
+        name:'nuevaE',
+        url:'/nuevaE',
+        templateUrl: './views/nuevaEntrada.html'
+    }
+    var listadoEntrada={
+        name:'listadoE',
+        url:'/listadoE',
+        templateUrl: './views/listadoEntrada.html'
+    }
+
+    var listadoEntradaDetalle={  //contenido del articulo(blog)
+        name:'listadoEDetalle',
+        url:'/listadoEDetalle',
+        templateUrl:'./views/listadoEntradaDetalle.html'
+    }
+
+    $stateProvider.state(nuevaEntrada);
+    $stateProvider.state(listadoEntrada);
+    $stateProvider.state(listadoEntradaDetalle);
+})
