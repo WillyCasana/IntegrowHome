4d869b646f8bb3395a23097be772703ac38b8831
Willy <willy@correomodificado.com>
2020-11-07 02:41:39 -0500

    registrar

--- FrontEndBlog/src/js/nuevaEntrada.js
+++ FrontEndBlog/src/js/nuevaEntrada.js
@@ -1,5 +1,10 @@
-app.controller('nuevaEntradaCller',function($scope){
+app.controller('nuevaEntradaCller',function($scope,$http){
     $scope.guardarEntrada=function(entrada){
         console.log(entrada);
-    }
+        $http({method:'POST', url:'https://localhost:44335/blog',data:entrada}).then(function successCallback(response){
+            console.log(response.data);
+        },function errorCallback(response){
+            console.log('Error');
+        });
+    };
 })
